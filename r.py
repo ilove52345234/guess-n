b8d0561db0b641c595a1d8a043af38b378efc289
Min Cheung <ilove52345234@gmail.com>
2020-01-06 11:34:59 +0800

    fix commit

--- r.py
+++ r.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1, 100)
+mn = input('請決定最小數字')
+mn = int(mn)
+mx = input('請決定最大數字')
+mx = int(mx)
+r = random.randint(mn, mx)
 x = 0
 while True:
     i = input('請輸入一個數字: ')
@@ -8,10 +12,11 @@ while True:
     if i == r:
         print('終於猜對了!')
         print('猜了第', x, '次')
-        break
+        break 
     elif i < r:
         print('比答案小!')
     elif i > r:
         print('比答案大!')
-
+    if i < mn or i > mx:
+        print('請輸入', mn, '到', mx, '的數字')
     print('猜了第', x, '次')
